e761dbac1e647ea70e4f81c4a1ad7a88bd777246
TotaBraz <tota.braz@gmail.com>
2019-10-21 15:44:23 -0300

    update 229

--- 03_http--01-starting-setup/src/containers/Blog/Blog.js
+++ 03_http--01-starting-setup/src/containers/Blog/Blog.js
@@ -9,7 +9,10 @@ import  './Blog.module.css'
 
 class Blog extends Component {
   
-    
+    constructor(props){
+        super(props);
+        this.state ={ }
+    }
 
     render () {
   
--- 03_http--01-starting-setup/src/containers/Blog/Posts/Posts.js
+++ 03_http--01-starting-setup/src/containers/Blog/Posts/Posts.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import axios from "../../../axios";
 import Post from '../../../components/Post/Post'
-
 import "./Posts.module.css";
-
 
 class Posts extends Component {
 
