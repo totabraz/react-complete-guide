ef893d943eb842b3c86103d06b29909abd753bca
Tota <hey@totabraz.com>
2020-01-30 12:33:31 -0300

    firebase hosting

--- 02_burger_basics_01/src/index.js
+++ 02_burger_basics_01/src/index.js
@@ -27,7 +27,7 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 const app = (
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/">
             <App/>
         </BrowserRouter>
     </Provider>
