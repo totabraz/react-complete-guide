22c409d8c73ed5a867a2bb6ed88f68f635f33d45
Tota <hey@totabraz.com>
2020-02-12 17:39:52 -0300

    update until 345

--- 08_animations--01-start/src/components/Modal/Modal.js
+++ 08_animations--01-start/src/components/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Modal.css';
-import { Transition } from "react-transition-group";
+import { CSSTransition } from "react-transition-group";
 
 
 const animationTime = {
@@ -10,32 +10,22 @@ const animationTime = {
 const modal = (props) => {
     console.log('props.show', props.show)
     return (
-        <Transition
+        <CSSTransition
             in={props.show}
             timeout={animationTime}
             mountOnEnter
             unmountOnExit
-            onEnter={() => { console.log('onEnter')}}
-            onEntering={() => { console.log('onEntering')}}
-            onEntered={() => { console.log('onEntered')}}
-            onExit={() => { console.log('onExit')}}
-            onExiting={() => { console.log('onExiting')}}
-            onExited={() => { console.log('onExited')}}
-            >
-            {state => {
-                const cssClass = [
-                    'Modal',
-                    state === "entering"
-                        ? 'ModalOpen'
-                        : state === 'exiting' ? 'ModalClosed' : null]
-                return (
-                    <div className={cssClass.join(' ')}>
-                        <h1>A Modal</h1>
-                        <button className="Button" onClick={props.closed}>Dismiss</button>
-                    </div>
-                )
-            }}
-        </Transition>
+
+            classNames={'fade-side'}
+        >
+            {() => (
+                <div className="Modal">
+                    <h1>A Modal</h1>
+                    <button className="Button" onClick={props.closed}>Dismiss</button>
+                </div>
+            )
+            }
+        </CSSTransition>
     )
 }
 
